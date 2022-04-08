e3da6277a6dcf09487a804f7be2fe62e47792173
r1ader <r1ader@qq.com>
2022-04-08 11:55:36 +0800

    design

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Main msg="Hello Vue 3 + Vite"/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<template>
+  <div class="main_container">
+    <div class="nav">
+    </div>
+    <div class="content">content</div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+.main_container {
+  width: 100vw;
+  display: flex;
+  /*justify-content: space-between;*/
+  flex-direction: row;
+}
+
+.nav {
+  height: 100vh;
+  background-color: rgba(189, 165, 165, 0.31);
+}
+
+.content {
+  width: auto;
+}
+</style>
